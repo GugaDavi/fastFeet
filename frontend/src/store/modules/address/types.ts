beb284e8580967782fe230de707431c7b0771572
Fliper Gustavo <gguedes.tech@gmail.com>
2020-04-07 19:35:52 -0300

    web: Address Type

---
+++ frontend/src/store/modules/address/types.ts
@@ -0,0 +1,12 @@
+/**
+ * Data Types
+ */
+
+export interface IAddress {
+  street: string;
+  house_number: string;
+  complement?: string;
+  state: string;
+  city: string;
+  zip_code: string;
+}
